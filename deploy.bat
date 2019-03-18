8da3d8504844ac3e976b474b70d21b1b3e5dfec0
Işık Kaplan <isik.kaplan@hotmail.com>
2019-03-18 19:12:20 +0300

    deployement

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,6 @@
+python3 -m pip install -r requirements.txt
 cd eksiclone
 python3 manage.py makemigrations mainsite
 python3 manage.py migrate
+python3 pre_deploy.py
 python3 deploy.py
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+python3 -m pip install -r requirements.txt
 cd eksiclone
-sudo python3 manage.py makemigrations mainsite
-sudo python3 manage.py migrate
+python3 manage.py makemigrations mainsite
+python3 manage.py migrate
+python3 pre_deploy.py
 sudo python3 deploy.py
