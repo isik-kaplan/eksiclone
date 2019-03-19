efb13308228b9ba5f0e990fcbdff24e597c1926c
Işık Kaplan <isik.kaplan@hotmail.com>
2019-03-19 11:38:11 +0300

    Fix titles problems

--- eksiclone/mainsite/views/general_views/titles.py
+++ eksiclone/mainsite/views/general_views/titles.py
@@ -4,13 +4,15 @@ from databases.mainsite.queries import DEFAULT_CHANNEL_QUERIES
 from mainsite.app_models import (
     TitleChannel,
 )
+
+from django.http.response import Http404
 from mainsite.urls import url
 from mainsite.views.view_mixins import (
     UrlMixin,
     PaginatorMixin,
 )
 from utils.debug import debug
-from utils.decorators import suppress_and_return
+from utils.decorators import suppress_and_raise
 
 
 @debug
@@ -19,7 +21,7 @@ class TitlesPage(ListView, UrlMixin, PaginatorMixin):
     template_name = 'mainsite/titles/titles.html'
     context_object_name = 'titles'
 
-    @suppress_and_return(TitleChannel.DoesNotExist, instead=TitleChannel.objects.none())
+    @suppress_and_raise(TitleChannel.DoesNotExist, instead=Http404())
     def get_queryset(self):
         if self.channel in DEFAULT_CHANNEL_QUERIES:
             query_set = DEFAULT_CHANNEL_QUERIES[self.channel]['query']
@@ -34,12 +36,13 @@ class TitlesPage(ListView, UrlMixin, PaginatorMixin):
         _paginate_by = getattr(user, 'title_pref', 10)
         return _paginate_by
 
-    def _extra_context(self):
+    def get_context_data(self, *, object_list=None, **kwargs):
         if self.channel in DEFAULT_CHANNEL_QUERIES:
             description = DEFAULT_CHANNEL_QUERIES[self.channel]['description']
         else:
             description = TitleChannel.from_url(self.channel).description
         return {
+            **super().get_context_data(object_list=object_list, **kwargs),
             'channel': self.channel,
             'description': description
         }
