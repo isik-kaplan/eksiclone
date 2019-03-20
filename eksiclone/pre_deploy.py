28e5a801360c368eed21e35f07dde01c514e5bf8
Işık Kaplan <isik.kaplan@hotmail.com>
2019-03-20 21:51:26 +0300

    Compress css

--- eksiclone/pre_deploy.py
+++ eksiclone/pre_deploy.py
@@ -10,6 +10,7 @@ print(application)
 def default_themes():
     Theme(name='standard', lights=True, is_built_in=True, path='mainsite/style/light.standard.css').save()
     Theme(name='standard', lights=False, is_built_in=True, path='mainsite/style/dark.standard.css').save()
+    Theme(name='orange', lights=False, is_built_in=True, path='mainsite/style/dark.orange.css').save()
 
 
 if __name__ == '__main__':
