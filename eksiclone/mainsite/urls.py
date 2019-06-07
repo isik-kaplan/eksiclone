e70d15df9f4acfb719a087703a82d3789c6b11dc
Işık Kaplan <isik.kaplan@hotmail.com>
2019-06-07 23:09:03 +0300

    fix message url

--- eksiclone/mainsite/urls.py
+++ eksiclone/mainsite/urls.py
@@ -6,7 +6,7 @@ mainsite_urls = UrlManager(views_root='mainsite.views')
 
 message_urlpatterns = [
     re_path(
-        r'^message/(?P<username>[a-zA-Z0-9-]+)$',
+        r'^message/(?P<username>[a-zA-Z0-9-]+)',
         views.DialogListView.as_view(),
         name='message'
     ),
