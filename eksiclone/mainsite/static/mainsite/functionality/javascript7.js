0de2e82fa411cac77f93d9d833907e740cbb7ef2
Atakan <me@atakanermis.me>
2020-05-09 15:09:39 +0300

    dotdotdot menu fixed

--- eksiclone/mainsite/static/mainsite/functionality/javascript7.js
+++ eksiclone/mainsite/static/mainsite/functionality/javascript7.js
@@ -1,7 +1,3 @@
-console.log(document.querySelector("#svg-dots"))
-
-console.log(document.querySelectorAll("#svg-dots"))
-
 document.querySelectorAll("#svg-dots").forEach(el => {
     el.addEventListener('click', e => {
         e.target.parentElement.nextElementSibling.classList.toggle('open')
